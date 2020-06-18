781cde4d41d2ddc5328b6e14a267d8f0c1a70ace
Noarison Léonce <trazafimiandrisoa@gmail.com>
2020-06-18 16:10:37 +0300

    Update licence

--- dimgray_backend_theme_v8/__openerp__.py
+++ dimgray_backend_theme_v8/__openerp__.py
@@ -10,7 +10,7 @@
     # Your information
     'author': 'RAZAFIMIANDRISOA Noarison Léonce',
     'website': 'kheluc@yahoo.com',
-    'license': 'OPL-1',
+    'license': 'Other proprietary',
     'summary': 'Dimgray Backend Theme V8',
     'images': [
         'images/screen.png'
